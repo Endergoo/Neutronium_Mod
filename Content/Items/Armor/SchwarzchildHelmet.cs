43a18ca544f12e03892082ad9e16020c71d038d9
Harrison Boone <hmboone@email.sc.edu>
2026-02-19 21:02:45 -0500

    asd

--- Content/Items/Armor/SchwarzchildHelmet.cs
+++ Content/Items/Armor/SchwarzchildHelmet.cs
@@ -55,7 +55,7 @@ namespace Neutronium.Content.Items.Armor
         {
             player.buffImmune[BuffID.OnFire] = true; // Make the player immune to Fire
             player.statLifeMax2 += 25;
-            player.statManaMax2 += MaxManaIncrease; // Increase how many mana points the player can have by 20
+            player.statManaMax2 += MaxManaIncrease;
             player.GetModPlayer<GlobalPlayer>().StellarDamage += 0.02f;
 
         }
