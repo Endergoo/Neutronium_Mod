3d52fd0c14f04e6ddc32c1ce5c67880ebd537b95
Harrison Boone <hmboone@email.sc.edu>
2026-03-04 20:02:43 -0500

    adas

--- Content/Projectiles/PygylyggJellyfish.cs
+++ Content/Projectiles/PygylyggJellyfish.cs
@@ -29,8 +29,8 @@ public class PygylyggJellyfish : ModProjectile
     public override void AI()
     {
         // Glowing jellyfish effect
-        Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Water, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
-        Lighting.AddLight(Projectile.Center, new Vector3(0.0f, 0.5f, 1.5f));
+        Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Electric, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
+        
 
     }
 
