b66edac0e9a30c634f6afec9976f01b5f3a0c217
Harrison Boone <hmboone@email.sc.edu>
2026-03-05 13:54:15 -0500

    fdsfs

--- Content/Items/Weapons/BookofCelestials.cs
+++ Content/Items/Weapons/BookofCelestials.cs
@@ -7,6 +7,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Graphics.CameraModifiers;
 using Terraria.DataStructures;
+using Neutronium.Content.Buffs;
 
 namespace Neutronium.Content.Items.Weapons
 {
