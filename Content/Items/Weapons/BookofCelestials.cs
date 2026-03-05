88c3def0fb68fe752fbbd66c74223501b956f3f0
Harrison Boone <hmboone@email.sc.edu>
2026-03-04 21:02:51 -0500

    asda

--- Content/Items/Weapons/BookofCelestials.cs
+++ Content/Items/Weapons/BookofCelestials.cs
@@ -4,7 +4,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 
-namespace Neutronium.Content.Items
+namespace Neutronium.Content.Items.Weapons
 {
     public class BookOfCelestials : ModItem
     {
