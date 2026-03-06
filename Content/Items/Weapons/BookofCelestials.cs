a2f894c9d5b1bec78a681520cdbf939e5d5ed030
Harrison Boone <hmboone@email.sc.edu>
2026-03-06 17:12:56 -0500

    might break it

--- Content/Items/Weapons/BookofCelestials.cs
+++ Content/Items/Weapons/BookofCelestials.cs
@@ -106,7 +106,7 @@ namespace Neutronium.Content.Items.Weapons
             Projectile.timeLeft = 600;
             Projectile.scale = 2.5f;
             Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 20;
+            Projectile.localNPCHitCooldown = -1;
             Projectile.DamageType = DamageClass.Magic;
         }
 
