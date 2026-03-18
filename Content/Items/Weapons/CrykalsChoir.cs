de4bf58acc8d8b77fb05fc2d2abcc669fc392454
Harrison Boone <hmboone@email.sc.edu>
2026-03-17 22:41:45 -0400

    ssss

--- Content/Items/Weapons/CrykalsChoir.cs
+++ Content/Items/Weapons/CrykalsChoir.cs
@@ -7,6 +7,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Graphics.CameraModifiers;
 using Terraria.DataStructures;
+using Neutronium.Content.Projectiles;
 
 namespace Neutronium.Content.Items.Weapons
 {
