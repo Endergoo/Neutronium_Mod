5fa5aa92a90d1bf5f1a641cf8dbf3ceb93e3933b
Harrison Boone <hmboone@email.sc.edu>
2026-03-19 13:42:17 -0400

    asda

--- Content/Items/Weapons/CrykalsChoir.cs
+++ Content/Items/Weapons/CrykalsChoir.cs
@@ -6,6 +6,7 @@ using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Neutronium.Core.Utils;
+using Neutronium.Content.Projectiles;
 
 namespace Neutronium.Content.Items.Weapons
 {
