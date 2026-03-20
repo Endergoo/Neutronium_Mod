3c6871837880b76e590f33f1fb583e110a9a343d
Harrison Boone <hmboone@email.sc.edu>
2026-03-20 17:12:04 -0400

    dsad

--- Content/Items/Weapons/CrykalsChoir.cs
+++ Content/Items/Weapons/CrykalsChoir.cs
@@ -71,7 +71,7 @@ namespace Neutronium.Content.Items.Weapons
             {
                 if (playSound)
                 {
-                    SoundStyle swing = new("Neutonium/Sounds/Item/Swoosh");
+                    SoundStyle swing = new("Neutronium/Sounds/Item/Swoosh");
                     SoundEngine.PlaySound(swing with { Pitch = Main.rand.NextFloat(0.1f, 0.3f), Volume = 1f }, player.Center);
                     playSound = false;
                 }
