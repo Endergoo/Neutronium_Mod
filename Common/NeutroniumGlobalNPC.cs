70fc1367e4e7191ad6e4a73fb3861b3b05faeb96
Harrison Boone <hmboone@email.sc.edu>
2026-03-23 20:57:44 -0400

    position changes

--- Common/NeutroniumGlobalNPC.cs
+++ Common/NeutroniumGlobalNPC.cs
@@ -9,13 +9,14 @@ using Neutronium.Content.Buffs.DoT;
 public class NeutroniumGlobalNPC : GlobalNPC
 {
     private const float DebuffIconScale = 0.5f;
+    private const float DebuffIconOffset = 16f;
 
     public override void PostDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
     {
         if (npc.HasBuff(ModContent.BuffType<AtomicDeconstruction>()))
         {
             Texture2D icon = ModContent.Request<Texture2D>("Neutronium/Content/Buffs/DoT/AtomicDeconstruction").Value;
-            Vector2 drawPos = npc.Top - screenPos - new Vector2(icon.Width * DebuffIconScale / 2f, icon.Height * DebuffIconScale + 4f);
+            Vector2 drawPos = npc.Top - screenPos - new Vector2(icon.Width * DebuffIconScale / 2f, icon.Height * DebuffIconScale + DebuffIconOffset);
             spriteBatch.Draw(icon, drawPos, null, Color.White, 0f, Vector2.Zero, DebuffIconScale, SpriteEffects.None, 0f);
         }
     }
