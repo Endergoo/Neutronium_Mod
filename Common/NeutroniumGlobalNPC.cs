3e53623967352e7809a48923fabd0316b9173ef6
Harrison Boone <hmboone@email.sc.edu>
2026-03-23 20:18:32 -0400

    Fixed pathing again

--- Common/NeutroniumGlobalNPC.cs
+++ Common/NeutroniumGlobalNPC.cs
@@ -14,7 +14,7 @@ namespace Neutronium.Common
         {
             if (npc.HasBuff(ModContent.BuffType<AtomicDeconstruction>()))
             {
-                Texture2D icon = ModContent.Request<Texture2D>("Neutronium/Content/Buffs/AtomicDeconstruction").Value;
+                Texture2D icon = ModContent.Request<Texture2D>("Neutronium/Content/Buffs/DoT/AtomicDeconstruction").Value;
                 Vector2 drawPos = npc.Top - screenPos - new Vector2(icon.Width / 2f, icon.Height + 4f);
                 spriteBatch.Draw(icon, drawPos, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
             }
