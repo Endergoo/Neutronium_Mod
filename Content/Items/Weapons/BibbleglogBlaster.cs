a0619a3166410737f0df2d48712ba890de2a1d6b
Harrison Boone <hmboone@email.sc.edu>
2026-03-24 17:30:56 -0400

    sound reverts

--- Content/Items/Weapons/BibbleglogBlaster.cs
+++ Content/Items/Weapons/BibbleglogBlaster.cs
@@ -6,7 +6,6 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.Audio;
 
 namespace Neutronium.Content.Items.Weapons
 {
@@ -26,6 +25,7 @@ namespace Neutronium.Content.Items.Weapons
             Item.knockBack = 3;
             Item.value = Item.buyPrice(gold: 2);
             Item.rare = ItemRarityID.Pink;
+            Item.UseSound = SoundID.Item122; 
             Item.autoReuse = true;
             Item.shoot = ModContent.ProjectileType<ChainLightningProj>();
             Item.shootSpeed = 18f;
@@ -39,14 +39,7 @@ namespace Neutronium.Content.Items.Weapons
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            SoundEngine.PlaySound(new SoundStyle("Terraria/Sounds/Thunder") with 
-            { 
-                Variants = new int[] { 0, 1, 2, 3, 4, 5, 6 },
-                SoundLimitBehavior = SoundLimitBehavior.ReplaceOldest,
-                Volume = 0.3f
-            }, player.Center);
-
-            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 60f + new Vector2(0f, -8f);
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 55f + new Vector2(0f, -8f);
             position += muzzleOffset;
 
             Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<ChainLightningProj>(), damage, knockback, player.whoAmI);
@@ -63,5 +56,3 @@ namespace Neutronium.Content.Items.Weapons
         }
     }
 }
-
-    
