11b350f7269791f6e27f5a5796ffde5d3acfa288
Harrison Boone <hmboone@email.sc.edu>
2026-03-23 23:38:52 -0400

    Recipe changes

--- Content/Items/Weapons/SpitefulPrick.cs
+++ Content/Items/Weapons/SpitefulPrick.cs
@@ -40,11 +40,10 @@ namespace Neutronium.Content.Items.Weapons
          public override void AddRecipes()
         {
             CreateRecipe()
-                .AddIngredient(ItemID.Stinger, 10)
-                .AddIngredient(ItemID.JungleSpores, 12)
-                .AddIngredient(ItemID.Vine, 2)
-                .AddTile(TileID.Anvils)
-                Register();
+                .AddIngredient(ItemID.Stinger, 10) 
+                .AddIngredient(ItemID.JungleSpores, 12) 
+                .AddTile(TileID.Anvils) 
+                .Register();
         }
     }
 }
