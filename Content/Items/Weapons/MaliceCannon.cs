5a3e5e9a2f2658a35271b04bbfdd08a50b87877a
Harrison Boone <hmboone@email.sc.edu>
2026-03-30 22:49:34 -0400

    s

--- Content/Items/Weapons/MaliceCannon.cs
+++ Content/Items/Weapons/MaliceCannon.cs
@@ -62,8 +62,8 @@ namespace Neutronium.Content.Items.Weapons
             }
 
             // Spawn the beam projectile
-            Vector2 muzzlePos = position + new Vector2(20f * player.direction, -8f);
-            
+            Vector2 muzzlePos = position + dir * 30f + new Vector2(0f, 0f);
+
             Projectile beam = Projectile.NewProjectileDirect(
                 source,
                 muzzlePos,
