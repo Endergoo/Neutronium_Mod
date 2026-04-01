02e6c213b5a4a08d8d0b8fb781f6254eebc93bab
Harrison Boone <hmboone@email.sc.edu>
2026-03-31 21:44:42 -0400

    das

--- Content/Projectiles/MaliceBeam.cs
+++ Content/Projectiles/MaliceBeam.cs
@@ -78,11 +78,12 @@ namespace Neutronium.Content.Projectiles
 
         private void SpawnExplosion(Vector2 position)
         {
-             SoundEngine.PlaySound(new SoundStyle("Neutronium/Content/Sounds/Items/MCannon") with
-            {
-                Pitch = Main.rand.NextFloat(-0.15f, 0.15f),
-                Volume = 0.5f
-            }, position);
+        SoundEngine.PlaySound(new SoundStyle("Neutronium/Content/Sounds/Items/Mcannon") with            
+        {
+            Pitch = Main.rand.NextFloat(-0.15f, 0.15f),
+            Volume = 0.5f
+        }, position);
+
             // Screen shake
             PunchCameraModifier punch = new PunchCameraModifier(
                 position, Main.rand.NextVector2Unit(), 8f, 10f, 20, 1000f);
