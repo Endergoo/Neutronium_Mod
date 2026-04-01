2acd30d29860b47b4c5654c612b6ac63d4d34240
Harrison Boone <hmboone@email.sc.edu>
2026-03-31 21:28:53 -0400

    fixed

--- Content/Projectiles/MaliceBeam.cs
+++ Content/Projectiles/MaliceBeam.cs
@@ -5,7 +5,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Graphics.CameraModifiers;
-using Neutronium.Content.Sound;
+using Neutronium.Content.Sounds;
 using Terraria.Audio;
 
 namespace Neutronium.Content.Projectiles
